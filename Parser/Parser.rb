b73f52a0361e738f9ca4e5f1b723393bf77d8908
Albin Dahlén <Albin@dahlen.ws>
2023-02-16 17:26:02 +0100

    Implemented a parser

--- Parser/Parser.rb
+++ Parser/Parser.rb
@@ -6,7 +6,7 @@ class Node
   end
 end
 
-class IntegerNode < Node
+class NumberNode < Node
   def initialize(value)
     @value = value
   end
@@ -27,7 +27,7 @@ class BinaryOpNode < Node
     left_val = @left.eval
     right_val = @right.eval
 
-    left_val.send(@op.to_sym, right_val)
+    left_val.send(@op, right_val)
   end
 end
 
@@ -35,26 +35,92 @@ end
 class Parser
   def initialize(tokens)
     @tokens = tokens
-    @current_index = 0
-    @current_token = @tokens[@current_index]
+    @current_index = -1
+    @current_token = nil
+    advance()
   end
 
   def parse
-    return expr()
+    node = nil
+    while !@current_token.type.eql?(TokenType::EOF)
+      node = expression()
+    end
+
+    return node
   end
 
   private
 
-  def consume(token_type)
-    if @current_token.type = token_type
-      @current_index += 1
-      @current_token = @tokens[@current_index]
-    else
-      error()
-    end
-  end 
+  def expression
+    node = term()
 
-  def expr()
+    while @current_token.type == TokenType::OPERATOR && [Operators::PLUS, Operators::MINUS].include?(@current_token.value)
+      token = @current_token
+      if token.value == Operators::PLUS
+        consume(TokenType::OPERATOR)
+        node = BinaryOpNode.new(node, token.value, term())
+      elsif token.value == Operators::MINUS
+        consume(TokenType::OPERATOR)
+        node = BinaryOpNode.new(node, token.value, term())
+      end
+    end
+
     return node
   end
+
+  def term
+    node = factor()
+
+    while @current_token.type == TokenType::OPERATOR && [Operators::MULTIPLY, Operators::DIVIDE].include?(@current_token.value)
+      token = @current_token
+      if token.value == Operators::MULTIPLY
+        consume(TokenType::OPERATOR)
+        node = BinaryOpNode.new(node, token.value, factor())
+      elsif token.value == Operators::DIVIDE
+        consume(TokenType::OPERATOR)
+        node = BinaryOpNode.new(node, token.value, factor())
+      end
+    end
+  
+    return node
+  end
+
+  def factor
+    token = @current_token
+    case token.type
+    when TokenType::INTEGER, TokenType::FLOAT
+      consume(token.type) # consume the number token
+      return NumberNode.new(token.value)
+    when TokenType::LPAREN
+      consume(TokenType::LPAREN) # consume the opening parenthesis token
+      expr = expression
+      consume(TokenType::RPAREN) # consume the closing parenthesis token
+      return expr
+    else
+      raise "Invalid syntax: expected a number or an expression in parentheses, but found #{token.value} at line #{token.line}, column #{token.column}"
+    end
+  end
+
+  def consume(token_type)
+    if @current_token.type == token_type
+      advance()
+    else
+      raise "Invalid syntax"
+    end
+  end
+
+  def advance
+    @current_index += 1
+    if @current_index < @tokens.length
+      @current_token = @tokens[@current_index]
+    end
+  end
+  
 end
+
+input = "3 * (4+3)"
+lexer = Lexer.new(input)
+tokens = lexer.tokenize
+
+parser = Parser.new(tokens)
+puts parser.parse().eval
