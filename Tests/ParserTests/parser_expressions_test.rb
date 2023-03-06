b1f6afde5021dc0d4b0d92779f8a758919ebed08
Albin Dahlén <Albin@dahlen.ws>
2023-03-06 21:32:49 +0100

    Moved Parser test files

---
+++ Tests/ParserTests/parser_expressions_test.rb
@@ -0,0 +1,242 @@
+require 'test/unit'
+require_relative '../../Parser/Parser.rb'
+
+class TestParserExpressions < Test::Unit::TestCase
+    def setup
+        @parser = Parser.new()
+    end
+
+    def test_parse_reassign_expression
+        ast = @parser.produceAST("x = 2")
+        assert_equal(ast.body[0].assigne.symbol, "x")
+        assert_equal(ast.body[0].value.value, 2)
+
+        # Try assign another variable to y
+        ast = @parser.produceAST("y = x")
+        assert_equal(ast.body[0].assigne.symbol, "y")
+        assert_equal(ast.body[0].value.symbol, "x")
+    end
+
+    def test_parse_binary_expression
+        ast = @parser.produceAST("1 + 2 * 3")
+        assert_equal(ast.body[0].left.value, 1)
+        assert_equal(ast.body[0].op, :+)
+        assert_equal(ast.body[0].right.left.value, 2)
+        assert_equal(ast.body[0].right.op, :*)
+        assert_equal(ast.body[0].right.right.value, 3)
+    end
+
+    def test_parse_addition_expression
+        ast = @parser.produceAST("1 + 2")
+        assert_equal(ast.body[0].left.value, 1)
+        assert_equal(ast.body[0].op, :+)
+        assert_equal(ast.body[0].right.value, 2)
+    end
+
+    def test_parse_subtraction_expression
+        ast = @parser.produceAST("10 - 5")
+        assert_equal(ast.body[0].left.value, 10)
+        assert_equal(ast.body[0].op, :-)
+        assert_equal(ast.body[0].right.value, 5)
+    end
+
+    def test_parse_multiplication_expression
+        ast = @parser.produceAST("3 * 4")
+        assert_equal(ast.body[0].left.value, 3)
+        assert_equal(ast.body[0].op, :*)
+        assert_equal(ast.body[0].right.value, 4)
+    end
+
+    def test_parse_division_expression
+        ast = @parser.produceAST("8 / 2")
+        assert_equal(ast.body[0].left.value, 8)
+        assert_equal(ast.body[0].op, :/)
+        assert_equal(ast.body[0].right.value, 2)
+    end
+
+    def test_parse_modulo_expression
+        ast = @parser.produceAST("7 % 3")
+        assert_equal(ast.body[0].left.value, 7)
+        assert_equal(ast.body[0].op, :%)
+        assert_equal(ast.body[0].right.value, 3)
+    end
+
+    def test_parse_unary_expression
+        ast = @parser.produceAST("-3")
+        assert_equal(NODE_TYPES[:UnaryExpr], ast.body[0].type)
+        assert_equal(:-, ast.body[0].op)
+        assert_equal(3, ast.body[0].left.value)
+
+        ast = @parser.produceAST("-3 * 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(NODE_TYPES[:UnaryExpr], ast.body[0].left.type)
+        assert_equal(:-, ast.body[0].left.op)
+        assert_equal(3, ast.body[0].left.left.value)
+        assert_equal(:*, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+
+        # Test unary on vars
+        ast = @parser.produceAST("-x")
+        assert_equal(NODE_TYPES[:UnaryExpr], ast.body[0].type)
+        assert_equal(:-, ast.body[0].op)
+        assert_equal(NODE_TYPES[:Identifier], ast.body[0].left.type)
+        assert_equal("x", ast.body[0].left.symbol)
+    end
+
+    def test_parens_grouping
+        ast = @parser.produceAST("(3 + 3) * 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:*, ast.body[0].op) # Is higher up in the ast so has lower precedence
+        assert_equal(4, ast.body[0].right.value)
+        additionExpr = ast.body[0].left # Extract the addition expr for easier testing
+        assert_equal(NODE_TYPES[:BinaryExpr], additionExpr.type)
+        assert_equal(3, additionExpr.left.value)
+        assert_equal(3, additionExpr.right.value)
+        assert_equal(:+, additionExpr.op)
+
+        ast = @parser.produceAST("(3 * (3 - 4)) * 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:*, ast.body[0].op) # Is higher up in the ast so has lower precedence
+        assert_equal(4, ast.body[0].right.value)
+        firstParens = ast.body[0].left # Extract the addition expr for easier testing
+        assert_equal(NODE_TYPES[:BinaryExpr], firstParens.type)
+        assert_equal(3, firstParens.left.value)
+        assert_equal(:*, firstParens.op)
+        secondParens = firstParens.right
+        assert_equal(NODE_TYPES[:BinaryExpr], secondParens.type)
+        assert_equal(3, secondParens.left.value)
+        assert_equal(4, secondParens.right.value)
+        assert_equal(:-, secondParens.op)
+    end
+
+    def test_operator_precedence
+        # Test addition and multiplication
+        ast = @parser.produceAST("1 + 2 * 3")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:+, ast.body[0].op)
+        assert_equal(1, ast.body[0].left.value)
+        multiplicationExpr = ast.body[0].right
+        assert_equal(NODE_TYPES[:BinaryExpr], multiplicationExpr.type)
+        assert_equal(:*, multiplicationExpr.op)
+        assert_equal(2, multiplicationExpr.left.value)
+        assert_equal(3, multiplicationExpr.right.value)
+
+        # Test addition and division
+        ast = @parser.produceAST("1 + 2 / 3")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:+, ast.body[0].op)
+        assert_equal(1, ast.body[0].left.value)
+        divisionExpr = ast.body[0].right
+        assert_equal(NODE_TYPES[:BinaryExpr], divisionExpr.type)
+        assert_equal(:/, divisionExpr.op)
+        assert_equal(2, divisionExpr.left.value)
+        assert_equal(3, divisionExpr.right.value)
+
+        # Test multiplication and division are read from left to right
+        ast = @parser.produceAST("1 * 2 / 3")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:/, ast.body[0].op)
+        multiplicationExpr = ast.body[0].left
+        assert_equal(NODE_TYPES[:BinaryExpr], multiplicationExpr.type)
+        assert_equal(:*, multiplicationExpr.op)
+        assert_equal(1, multiplicationExpr.left.value)
+        assert_equal(2, multiplicationExpr.right.value)
+        assert_equal(3, ast.body[0].right.value)
+
+        # Test addition, divition with unary
+        ast = @parser.produceAST("1 + 2 / -3")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(:+, ast.body[0].op)
+        assert_equal(1, ast.body[0].left.value)
+        divisionExpr = ast.body[0].right
+        assert_equal(NODE_TYPES[:BinaryExpr], divisionExpr.type)
+        assert_equal(:/, divisionExpr.op)
+        assert_equal(2, divisionExpr.left.value)
+        unaryExpr = divisionExpr.right
+        assert_equal(NODE_TYPES[:UnaryExpr], unaryExpr.type)
+        assert_equal(:-, unaryExpr.op)
+        assert_equal(3, unaryExpr.left.value)
+    end
+
+    def test_invalid_expression
+        assert_raise(InvalidTokenError) {@parser.produceAST("4 + 3 *")}
+    end
+
+        def test_parse_less_than_expression
+        ast = @parser.produceAST("3 < 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:<, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_less_than_or_equal_to_expression
+        ast = @parser.produceAST("3 <= 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:<=, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_greater_than_expression
+        ast = @parser.produceAST("3 > 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:>, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_greater_than_or_equal_to_expression
+        ast = @parser.produceAST("3 >= 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:>=, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_equality_expression
+        ast = @parser.produceAST("3 == 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:==, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_inequality_expression
+        ast = @parser.produceAST("3 != 4")
+        assert_equal(NODE_TYPES[:BinaryExpr], ast.body[0].type)
+        assert_equal(3, ast.body[0].left.value)
+        assert_equal(:!=, ast.body[0].op)
+        assert_equal(4, ast.body[0].right.value)
+    end
+
+    def test_parse_logical_and_expression
+        ast = @parser.produceAST("true && false")
+        assert_equal(NODE_TYPES[:LogicalAnd], ast.body[0].type)
+        assert_equal(:"&&", ast.body[0].op)
+        assert_equal(true, ast.body[0].left.value)
+        assert_equal(false, ast.body[0].right.value)
+    end
+
+    def test_parse_logical_or_expression
+        ast = @parser.produceAST("true || false")
+        assert_equal(NODE_TYPES[:LogicalOr], ast.body[0].type)
+        assert_equal(:"||", ast.body[0].op)
+        assert_equal(true, ast.body[0].left.value)
+        assert_equal(false, ast.body[0].right.value)
+    end
+
+    def test_parse_logical_not_expression
+        ast = @parser.produceAST("!true")
+        assert_equal(NODE_TYPES[:UnaryExpr], ast.body[0].type)
+        assert_equal(:!, ast.body[0].op)
+        assert_equal(true, ast.body[0].left.value)
+
+        ast = @parser.produceAST("!(true && false)")
+        assert_equal(NODE_TYPES[:UnaryExpr], ast.body[0].type)
+        assert_equal(:!, ast.body[0].op)
+        assert_equal(NODE_TYPES[:LogicalAnd], ast.body[0].left.type)
+        assert_equal(true, ast.body[0].left.left.value)
+        assert_equal(false, ast.body[0].left.right.value)
+    end
+end
---
+++ Tests/ParserTests/parser_var_declaration_test.rb
@@ -0,0 +1,71 @@
+require 'test/unit'
+require_relative '../../Parser/Parser.rb'
+
+class TestParserVarDeclarations < Test::Unit::TestCase
+    def setup
+        @parser = Parser.new()
+    end
+
+    def test_parse_variable_declaration
+        ast = @parser.produceAST("int x = 1")
+        assert_equal(ast.body[0].identifier, "x")
+        assert_equal(ast.body[0].value.value, 1)
+        assert_equal(ast.body[0].constant, false)
+
+        # Test for declaration of var to value of another var
+        ast = @parser.produceAST("int a = x")
+        assert_equal(ast.body[0].identifier, "a")
+        assert_equal(ast.body[0].value.symbol, "x")
+        assert_equal(ast.body[0].constant, false)
+    end
+
+    def test_parse_variable_declaration_without_assign
+        ast = @parser.produceAST("int y")
+        assert_equal(ast.body[0].identifier, "y")
+        assert_equal(ast.body[0].value, nil)
+        assert_equal(ast.body[0].constant, false)
+
+        ast = @parser.produceAST("float v")
+        assert_equal(ast.body[0].identifier, "v")
+        assert_equal(ast.body[0].value, nil)
+        assert_equal(ast.body[0].constant, false)
+        assert_equal(ast.body[0].value_type, "float")
+    end
+
+    def test_parse_constant_declaration
+        ast = @parser.produceAST("const int x = 1")
+        assert_equal(ast.body[0].identifier, "x")
+        assert_equal(ast.body[0].value.value, 1)
+        assert_equal(ast.body[0].constant, true)
+    end
+
+    def test_parse_missing_value
+        assert_raise(InvalidTokenError) { @parser.produceAST("float x = ") }
+    end
+
+    def test_parse_missing_identifier
+        assert_raise(RuntimeError) { @parser.produceAST("int = 1") }
+    end
+
+    def test_parse_mismatched_type_on_var_declaration
+        assert_raise(InvalidTokenError) { @parser.produceAST("int a = true") }
+        assert_raise(InvalidTokenError) { @parser.produceAST("bool a = 40") }
+    end
+            
+    def test_parse_unknown_token
+        assert_raise(InvalidTokenError) { @parser.produceAST("int x @ 1") }
+    end
+
+    def test_parse_missing_type_specifier_on_constant
+        assert_raise(RuntimeError) { @parser.produceAST("const a = 1") }
+    end
+
+    def test_parse_missing_value_on_constant
+        assert_raise(NameError) { @parser.produceAST("const float x ") }
+    end
+
+    def test_parse_declaring_with_invalid_var_names
+        assert_raise(RuntimeError) { @parser.produceAST("int if = 4") }
+        assert_raise(RuntimeError) { @parser.produceAST("int 123 = 4") }
+    end
+end
