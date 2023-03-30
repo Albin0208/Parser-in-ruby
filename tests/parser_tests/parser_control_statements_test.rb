c9e94fdae36c8087d42e3e904fcf7087cd2ab4a5
Albin Dahlén <albin@dahlen.ws>
2023-03-30 17:10:50 +0200

    Added tests for elsif in parser testing

--- tests/parser_tests/parser_control_statements_test.rb
+++ tests/parser_tests/parser_control_statements_test.rb
@@ -45,6 +45,34 @@ class TestParserControlStatements < Test::Unit::TestCase
     assert_equal(NODE_TYPES[:BinaryExpr], conditions.right.type)
   end
 
+  def test_parse_single_elsif_statements
+    # Test if with one else if
+    ast = @parser.produce_ast('if 3 < 4 { int a = 4} elsif 3 > 4 {int a = 4 * 3}')
+    assert_equal(NODE_TYPES[:IF], ast.body[0].type)
+    assert_nil(ast.body[0].else_body)
+    assert_not_nil(ast.body[0].elsif_stmts)
+    conditions = ast.body[0].elsif_stmts[0].conditions
+    assert_equal(NODE_TYPES[:BinaryExpr], conditions.type)
+    body = ast.body[0].elsif_stmts[0].body
+    assert_equal(NODE_TYPES[:VarDeclaration], body[0].type)
+    assert_equal('a', body[0].identifier)
+    assert_equal(4, body[0].value.left.value)
+  end
+
+  def test_parse_multiple_elsif_statements
+    ast = @parser.produce_ast('if 3 < 4 { int a = 4} elsif 3 > 4 {int a = 4 * 3} elsif 3 == 4 { 3 + 3}')
+    assert_equal(NODE_TYPES[:IF], ast.body[0].type)
+    assert_nil(ast.body[0].else_body)
+    assert_not_nil(ast.body[0].elsif_stmts)
+    assert_equal(2, ast.body[0].elsif_stmts.length) # Make sure we have 2 elsifs
+
+    ast = @parser.produce_ast('if 3 < 4 { int a = 4} elsif 3 > 4 {int a = 4 * 3} elsif 3 == 4 { 3 + 3} elsif 3 == 4 { 3 + 3}')
+    assert_equal(NODE_TYPES[:IF], ast.body[0].type)
+    assert_nil(ast.body[0].else_body)
+    assert_not_nil(ast.body[0].elsif_stmts)
+    assert_equal(3, ast.body[0].elsif_stmts.length) # Make sure we have 2 elsifs
+  end
+
   def test_parse_if_else_statement
     ast = @parser.produce_ast('if 3 < 4 { int a = 4} else {int a = 4 * 3}')
     assert_equal(NODE_TYPES[:IF], ast.body[0].type)
@@ -53,5 +81,12 @@ class TestParserControlStatements < Test::Unit::TestCase
     assert_equal(NODE_TYPES[:VarDeclaration], body[0].type)
     assert_equal('a', body[0].identifier)
     assert_equal(4, body[0].value.left.value)
+  end
+  
+  def test_parse_if_elsif_else_statement
+    ast = @parser.produce_ast('if 3 < 4 { int a = 4} elsif 3 > 4 { 3 + 3 } else {int a = 4 * 3}')
+    assert_equal(NODE_TYPES[:IF], ast.body[0].type)
+    assert_not_nil(ast.body[0].else_body)
+    assert_not_nil(ast.body[0].elsif_stmts)
   end
 end
