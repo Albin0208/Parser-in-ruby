bdab095798b1d86f6ca4a9a0da0ff06dfe3e46b8
Albin Dahlén <albin@dahlen.ws>
2023-03-31 18:55:15 +0200

    Added functions test file

---
+++ tests/parser_tests/parser_functions_test.rb
@@ -0,0 +1,73 @@
+require 'test/unit'
+require_relative '../../parser/parser'
+
+class TestParserFunctions < Test::Unit::TestCase
+  def setup
+    @parser = Parser.new
+  end
+
+  def test_parse_missing_identifier
+    assert_raise(RuntimeError) { @parser.produce_ast('func int () {}') }
+    assert_raise(RuntimeError) { @parser.produce_ast('func int {}') }
+  end
+
+  def test_parse_missing_func_body
+    assert_raise(RuntimeError) { @parser.produce_ast('func int a()') }
+  end
+
+  def test_parse_mismatched_type_on_var_declaration
+    assert_raise(InvalidTokenError) { @parser.produce_ast('int a = true') }
+    assert_raise(InvalidTokenError) { @parser.produce_ast('bool a = 40') }
+  end
+
+  def test_parse_unknown_token
+    assert_raise(InvalidTokenError) { @parser.produce_ast('int x @ 1') }
+  end
+
+  def test_parse_missing_type_specifier
+    assert_raise(RuntimeError) { @parser.produce_ast('func a() {}') }
+  end
+
+  def test_parse_declaring_with_invalid_func_names
+    assert_raise(RuntimeError) { @parser.produce_ast('func void if() {}') }
+    assert_raise(RuntimeError) { @parser.produce_ast('func void 123() {}') }
+  end
+
+  def test_parse_void_function
+    ast = @parser.produce_ast('func void test() { 1 }')
+    body = ast.body[0]
+    assert_instance_of(FuncDeclaration, body)
+    assert_equal(body.identifier, 'test')
+    assert_equal(body.type_specifier, 'void')
+    assert_nil(body.params)
+    func_body = body.body[0]
+    assert_equal(1, func_body.value)
+  end
+
+  def test_parse_type_func_without_return_stmt
+    assert_raise(RuntimeError) { @parser.produce_ast('func int test() {}') }
+  end
+
+  def test_parse_func_with_return_stmt
+    ast = @parser.produce_ast('func int test() { return 1 }')
+    body = ast.body[0]
+    assert_instance_of(FuncDeclaration, body)
+    assert_equal(body.identifier, 'test')
+    assert_equal(body.type_specifier, 'int')
+    assert_nil(body.params)
+    func_body = body.body[0]
+    assert_instance_of(ReturnStmt, func_body)
+    return_body = func_body.body[0]
+    assert_instance_of(NumericLiteral, return_body)
+    assert_equal(1, return_body.value)
+  end
+
+  def test_parse_func_call
+    ast = @parser.produce_ast('test()')
+    body = ast.body[0]
+    puts body
+    assert_instance_of(CallExpr, body)
+    assert_equal('test', body.func_name.symbol)
+    assert_nil(body.params)
+  end
+end
