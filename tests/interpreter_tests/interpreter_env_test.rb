ad9d7d4654a9f082dd37b1000ee129945e81b364
Albin Dahlén <Albin@dahlen.ws>
2023-04-07 16:41:31 +0200

    Added some tests for env

---
+++ tests/interpreter_tests/interpreter_env_test.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require_relative '../../runtime/interpreter'
+
+class TestInterpreterExpr < Test::Unit::TestCase
+  def setup
+    @parser = Parser.new()
+    @interpreter = Interpreter.new
+    @env = Environment.new
+  end
+
+  def test_evaluate_var_declared_in_if
+    input = "if true { int a = 2 }
+             a"
+    ast = @parser.produce_ast(input)
+    assert_raise(RuntimeError) { @interpreter.evaluate(ast, @env)}
+  end
+
+  def test_evaluate_redeclaring_var_int_if
+    input = "int a = 2
+             if true { int a = 2 }"
+    ast = @parser.produce_ast(input)
+    assert_raise(RuntimeError) { @interpreter.evaluate(ast, @env)}
+  end
+
+  def test_evaluate_changing_var_in_if
+    input = "int a = 2
+             if true { a = 20 }
+             a"
+    ast = @parser.produce_ast(input)
+    result = @interpreter.evaluate(ast, @env)
+    assert_equal(20, result.value)
+  end
+end
