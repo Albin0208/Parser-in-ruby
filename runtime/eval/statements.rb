838d8f8d3d5adeed9069d1135ba10a02468c9412
Albin Dahlén <albin@dahlen.ws>
2023-04-20 14:43:47 +0200

    Added doc comment to eval_condition

--- runtime/eval/statements.rb
+++ runtime/eval/statements.rb
@@ -77,7 +77,7 @@ end
 #
 # Evaluates a condition, For example for a if statement
 #
-# @param [<Type>] condition The condition to be evaluated
+# @param [Expr | NullLiteral] condition The condition to be evaluated
 # @param [Environment] env The current environment
 #
 # @return [Boolean] True or false depinding on the result of the condition
