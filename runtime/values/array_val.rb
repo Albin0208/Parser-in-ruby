fdf3180ad305cce5ad04a2c04fae5249ba049b2b
Albin Dahlén <albin@dahlen.ws>
2023-05-17 17:49:54 +0200

    Added examples to array_val

--- runtime/values/array_val.rb
+++ runtime/values/array_val.rb
@@ -1,6 +1,12 @@
 module Runtime
   module Values
     # Represents a Array value in the language
+    # @example Creating an empty array
+    #   int[] a                 #=> []
+    #   string[] a = string[]{} #=> []
+    # @example Creating an with values array
+    #   int[] a = int[]{1, 2, 3}        #=> [1, 2, 3]
+    #   string[] a = string[]{"one", "two", "three"} #=> ["one", "two", "three"]
     class ArrayVal < RunTimeVal
       attr_reader :value_type
 
@@ -16,6 +22,10 @@ module Runtime
       # Checks whether this ArrayVal instance is equal to another RunTimeVal instance.
       # @param [RunTimeVal] other The other RunTimeVal instance to compare.
       # @return [BooleanVal] True if the two instances are equal, False otherwise.
+      # @example
+      #   int[]{1, 2} == int[]{1, 2, 3} #=> false
+      #   int[]{1, 2, 3} == int[]{1, 2, 3} #=> true
+      #   string[]{"1", "2", "3"} == int[]{1, 2, 3} #=> false
       def ==(other)
         return Values::BooleanVal.new(false) if @type != other.type
         return Values::BooleanVal.new(false) if @value.length != other.value.length
@@ -30,6 +40,10 @@ module Runtime
       # Checks whether this ArrayVal instance is not equal to another RunTimeVal instance.
       # @param [RunTimeVal] other The other RunTimeVal instance to compare.
       # @return [BooleanVal] True if the two instances are not equal, False otherwise.
+      # @example
+      #   int[]{1, 2} != int[]{1, 2, 3} #=> true
+      #   int[]{1, 2, 3} != int[]{1, 2, 3} #=> false
+      #   string[]{"1", "2", "3"} != int[]{1, 2, 3} #=> true
       def !=(other)
         Values::BooleanVal.new(!(self == other).value)
       end
@@ -38,6 +52,12 @@ module Runtime
       #
       # @param [RunTimeVal] append_value The value to be appended to the array.
       # @return [ArrayVal] The modified array with the appended element.
+      #
+      # @example
+      #   int[] a = int[]{1, 2, 3}
+      #   a.append(45) #=> [1, 2, 3, 45]
+      #
+      #   a.append(45).append(37) #=> [1, 2, 3, 45, 45, 37]
       def append(append_value)
         @value << append_value
         return self
@@ -46,6 +66,10 @@ module Runtime
       # Removes and returns the last element of the array.
       #
       # @return [RunTimeVal] The removed element.
+      # @example
+      #   int[] a = int[]{1, 2, 3}
+      #   a.pop() #=> 3
+      #   a       #=> [1, 2]
       def pop
         @value.pop()
       end
@@ -54,6 +78,10 @@ module Runtime
       #
       # @param [NumberVal] index The index of the element to be removed.
       # @return [RunTimeVal] The removed element.
+      # @example
+      #   int[] a = int[]{1, 2, 3}
+      #   a.remove_at(1) #=> 2
+      #   a       #=> [1, 3]
       def remove_at(index)
         raise "Error: Can't remove element at index of non-int type" unless index.is_a?(NumberVal) && index.type == :int
 
@@ -63,6 +91,9 @@ module Runtime
       # Returns the length of the array.
       #
       # @return [NumberVal] The length of the array.
+      # @example
+      #   int[] a = int[]{1, 2, 3}
+      #   a.length() #=> 3
       def length
         Values::NumberVal.new(@value.length(), :int)
       end
@@ -70,6 +101,9 @@ module Runtime
       # Returns a string representation of the array.
       #
       # @return [String] The string representation of the array.
+      # @example
+      #   int[] a = int[]{1, 2, 3}
+      #   a.to_s() #=> "[1, 2, 3]"
       def to_s
         string = '['
         @value.each() { |val| 
