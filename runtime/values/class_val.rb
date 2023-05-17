70cd7776c07280aecfc8c23e90a468cd17e95893
Albin Dahlén <albin@dahlen.ws>
2023-05-17 18:41:32 +0200

    Added examples to class_val

--- runtime/values/class_val.rb
+++ runtime/values/class_val.rb
@@ -3,6 +3,14 @@ module Runtime
     #
     # The object containing the custom class object
     #
+    # @example
+    #   class A {
+    #     int c
+    #     constructor(int _c) {
+    #       c = _c
+    #     }
+    #   }
+    #   A a = new A(2)
     class ClassVal < RunTimeVal
       attr_reader :class_instance
 
@@ -19,6 +27,11 @@ module Runtime
       #
       # @param [RunTimeVal] other The other value to compare with.
       # @return [BooleanVal] A BooleanVal indicating whether the given value is not equal to this value.
+      # @example
+      #   ClassA a = new ClassA()
+      #   ClassB b = new ClassB()
+      #   a != b #=> true
+      #   a != a #=> false
       def !=(other)
         Values::BooleanVal.new(!(other == self))
       end
@@ -27,6 +40,11 @@ module Runtime
       #
       # @param [RunTimeVal] other The other value to compare with.
       # @return [BooleanVal] A BooleanVal indicating whether the given value is equal to this value.
+      # @example
+      #   ClassA a = new ClassA()
+      #   ClassB b = new ClassB()
+      #   a == b #=> false
+      #   a == a #=> true
       def ==(other)
         return Values::BooleanVal.new(true) if other.object_id.equal?(self.object_id)
 
@@ -36,6 +54,14 @@ module Runtime
       # Returns a new instance of ClassVal that is a copy of this instance.
       #
       # @return [ClassVal] A new instance of ClassVal that is a copy of this instance.
+      # @example
+      #   ClassA a = new Class()
+      #   a.c = 4
+      #   ClassA b = a.copy()
+      #   b.c #=> 4
+      #   b.c = 2
+      #   a.c #=> 4
+      #   b.c #=> 2
       def copy
         Values::ClassVal.new(@value, @class_instance.clone)
       end
