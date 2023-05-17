fad7afbf16aa4d024cb71bb7fbfb69b08e970f5c
Albin Dahlén <albin@dahlen.ws>
2023-05-17 18:24:26 +0200

    Added examples for boolean_val

--- runtime/values/boolean_val.rb
+++ runtime/values/boolean_val.rb
@@ -2,7 +2,9 @@ module Runtime
   module Values
     #
     # The object representing a boolean value
-    #
+    # @example
+    #   bool a = true
+    #   bool b = false
     class BooleanVal < RunTimeVal
     # Creates a new boolean value object.
     #
@@ -14,6 +16,10 @@ module Runtime
     # Returns a new boolean value object with the opposite value of the current object.
     #
     # @return [BooleanVal] a new boolean value object with the opposite value of the current object
+    # @example
+    #   bool a = !true 
+    #   a #=> false
+    #   !a #=> true
     def !
       Values::BooleanVal.new(!@value)
     end
@@ -22,6 +28,14 @@ module Runtime
     #
     # @param other [RunTimeVal] the value to compare with
     # @return [BooleanVal] a new boolean value object indicating whether the values are unequal
+    # @example
+    #   true != true #=> false
+    #   true != false #=> true
+    #   false != false #=> false
+    #
+    #   bool a = true
+    #   bool b = false
+    #   a != b #=> true
     def !=(other)
       Values::BooleanVal.new(@value != other.value)
     end
@@ -30,6 +44,15 @@ module Runtime
     #
     # @param other [RunTimeVal] the value to compare with
     # @return [BooleanVal] a new boolean value object indicating whether the values are equal
+    #
+    # @example
+    #   true == true #=> true
+    #   true == false #=> false
+    #   false == false #=> true
+    #
+    #   bool a = true
+    #   bool b = false
+    #   a == b #=> false
     def ==(other)
       Values::BooleanVal.new(@value == other.value)
     end
