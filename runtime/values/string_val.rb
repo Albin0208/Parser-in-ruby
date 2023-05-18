cea8f79b8665a5c7e9bdd00a15533b94d40def9d
Albin Dahlén <albin@dahlen.ws>
2023-05-18 14:33:48 +0200

    Added examples for stringval

--- runtime/values/string_val.rb
+++ runtime/values/string_val.rb
@@ -3,6 +3,13 @@ module Runtime
     #
     # The object for representing a string
     #
+    # @example
+    #   string s = "hej"
+    #   string s2 = 'hej'
+    #   string s3 = "hello mate, how it go?"
+    #   s  #=> "hej"
+    #   s2 #=> "hej"
+    #   s3 #=> "hello mate, how it go?"
     class StringVal < RunTimeVal
       def initialize(value)
         super(value, :string)
@@ -13,6 +20,9 @@ module Runtime
       #
       # @param [RunTimeVal] other The runtime value to add to this one.
       #
+      # @example
+      #   "hello" + "world" #=> "helloworld"
+      #   'hello' + ' ' + "world" #=> "hello world"
       def +(other)
         Values::StringVal.new(@value + other.value)
       end
@@ -22,7 +32,11 @@ module Runtime
       #
       # @param [RunTimeVal] other The runtime value to multiply this one by.
       #
+      # @example
+      #   "hello" * 3 #=> "hellohellohello"
       def *(other)
+        raise "Error: Can't multiply withn non-integer value. Expected int but got #{other.type}" unless other.is_a?(NumberVal) && other.type == :int
+
         Values::StringVal.new(@value * other.value)
       end
 
@@ -31,6 +45,11 @@ module Runtime
       #
       # @param [RunTimeVal] other The runtime value to compare this one to.
       #
+      # @example
+      #   "hello" != "world" #=> true
+      #   "hello" != "hello" #=> false
+      #   "hello" != 'hello' #=> false
+      #   "hello" != 'world' #=> true
       def !=(other)
         Values::BooleanVal.new(@value != other.value)
       end
@@ -40,6 +59,11 @@ module Runtime
       #
       # @param [RunTimeVal] other The runtime value to compare this one to.
       #
+      # @example
+      #   "hello" == "world" #=> false
+      #   "hello" == "hello" #=> true
+      #   "hello" == 'hello' #=> true
+      #   "hello" == 'world' #=> false
       def ==(other)
         Values::BooleanVal.new(@value == other.value)
       end
@@ -49,6 +73,11 @@ module Runtime
       #
       # @return [NumberVal] The length of the string
       #
+      # @example
+      #   "hello".length() #=> 5
+      #   'world'.length() #=> 5
+      #   string a = "Hello world"
+      #   a.length()       #=> 11
       def length
         Values::NumberVal.new(@value.length, :int)
       end
